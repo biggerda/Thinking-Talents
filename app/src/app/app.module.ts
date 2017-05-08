0330b09f86a2fe1a9b14481525645f6814b86d51
Zae Holland <zaphane.holland@gmail.com>
2017-05-07 21:07:13 -0400

    adds thinkingTalents module and teamMap component

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TeamMapComponent } from './team-map/team-map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeamMapComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/src/app/team-map/team-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-map',
+  templateUrl: './team-map.component.html',
+  styleUrls: ['./team-map.component.css']
+})
+export class TeamMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
