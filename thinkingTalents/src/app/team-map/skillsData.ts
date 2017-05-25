7f63d31b64be54a0a87545f22720ce687a8b4bf1
Zae Holland <zaphane.holland@gmail.com>
2017-05-24 22:54:51 -0400

    add thinking talents to skills array

---
+++ thinkingTalents/src/app/team-map/skillsData.ts
@@ -0,0 +1,145 @@
+let skills = [
+    {
+        "name": "Achieving",
+        "description": "Excellence and efficiency are the measure- doing the best with the least."
+    },
+    {
+        "name": "Adapting",
+        "description": "Flexible, adjusts easily to change; expects and enjoys detours."
+    },
+    {
+        "name": "Believing",
+        "description": "Makes decisions based on values. People know where this person stands."
+    },
+    {
+        "name": "Collecting",
+        "description": "Collects anything that is deemed interesting."
+    },
+    {
+        "name": "Competing",
+        "description": "Compares performance to that of others."
+    },
+    {
+        "name": "Connecting",
+        "description": "Perceives the relationship between things and/or people."
+    },
+    {
+        "name": "Creating",
+        "description": "Intimacy Encourages deepening of relationships; comfortable with intimacy."
+    },
+    {
+        "name": "Enrolling",
+        "description": "Needs balance and to know that people are being treated in the same way."
+    },
+    {
+        "name": "Equalizing",
+        "description": "Needs balance and to know that people are being treated in the same way."
+    },
+    {
+        "name": "Feeling For Others",
+        "description": "Senses emotions in those nearby; anticipates others’ needs."
+    },
+    {
+        "name": "Fixing",
+        "description": "It Loves to identify what’s wrong and repair it or anticipate what might go wrong and avert it."
+    },
+    {
+        "name": "Focusing",
+        "description": "Capacity to concentrate on a goal for a long period of time"
+    },
+    {
+        "name": "Goal Setting",
+        "description": "Has daily drive to accomplish and meet a goal."
+    },
+    {
+        "name": "Having Confidence",
+        "description": "Knows he or she is able to deliver. Self-assured."
+    },
+    {
+        "name": "Including",
+        "description": "Desires to make others part of the group so as many as possible can feel its support."
+    },
+    {
+        "name": "Individuality",
+        "description": "Keen observer and intrigued with others’ unique style, relationships, talents and strengths."
+    },
+    {
+        "name": "Innovating",
+        "description": "Loves to create new processes or products; easily bored with routine."
+    },
+    {
+        "name": "Joking",
+        "description": "Can lighten tense moments and puts self and others at ease with laughter."
+    },
+    {
+        "name": "Love of Learning",
+        "description": "Energized by the journey from ignorance to competence."
+    },
+    {
+        "name": "Loving",
+        "description": "Ideas Searches for concepts to explain things; loves theories."
+    },
+    {
+        "name": "Making It Happen",
+        "description": "Impatient for action rather than contemplation."
+    },
+    {
+        "name": "Mentoring",
+        "description": "Goal is to help others achieve and grow."
+    },
+    {
+        "name": "Optimism",
+        "description": "Always on the lookout for the positive."
+    },
+    {
+        "name": "Organizing",
+        "description": "Enjoys managing and aligning many variables into the best configuration."
+    },
+    {
+        "name": "Peacemaking",
+        "description": "Looks for areas of agreement; holds conflict to a minimum."
+    },
+    {
+        "name": "Perfecting",
+        "description": "Imposes structure, sets up routines, timelines and deadlines; needs to feel in control."
+    },
+    {
+        "name": "Reliability",
+        "description": "Excuses and rationalizations are not acceptable; reputation for dependability."
+    },
+    {
+        "name": "Standing Out",
+        "description": "Wants to be known for making a difference and be admired for credibility."
+    },
+    {
+        "name": "Storytelling",
+        "description": "Needs to explain by painting vivid pictures."
+    },
+    {
+        "name": "Strategizing",
+        "description": "Sorts through clutter; recognizes all the possible options."
+    },
+    {
+        "name": "Taking Charge",
+        "description": "Uses confrontation if necessary and naturally directs others into action."
+    },
+    {
+        "name": "Thinking",
+        "description": "Alone Poses questions to self and tries to figure them out; constant mental hum."
+    },
+    {
+        "name": "Thinking Back",
+        "description": "Looks back to understand the present and future."
+    },
+    {
+        "name": "Thinking Forward",
+        "description": "Fascinated by the future; cherishes visions that energize and give hope."
+    },
+    {
+        "name": "Thinking Logically",
+        "description": "Exposes clumsy thinking; sees patterns in data."
+    }
+
+];
+
+export default skills;
--- thinkingTalents/src/app/team-map/team-map.component.ts
+++ thinkingTalents/src/app/team-map/team-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import skills from './skillsData';
 
 @Component({
   selector: 'app-team-map',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamMapComponent implements OnInit {
 
-  constructor() { }
+  skillsList: Array<skill> = skills;
+
+  constructor() {
+    console.log(this.skillsList);
+   }
 
   ngOnInit() {
   }
 
 }
+
+interface skill {
+  name: string;
+  description: string;
+}
+
