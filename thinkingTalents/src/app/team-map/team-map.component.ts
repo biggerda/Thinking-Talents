942713ddb7536b9ebc517445fc139c41e9ddebb3
Zae Holland <zaphane.holland@gmail.com>
2017-06-01 01:45:42 -0400

    added crud operations for team members
    began creating team map
    
    adds puzzle icons

--- thinkingTalents/src/app/team-map/team-map.component.ts
+++ thinkingTalents/src/app/team-map/team-map.component.ts
@@ -11,6 +11,7 @@ export class TeamMapComponent implements OnInit {
   skillsList: Array<skill> = skills;
   team: team;
   currentPlayer: player;
+  mode: string;
 
   constructor() {
     console.log(this.skillsList);
@@ -26,23 +27,63 @@ export class TeamMapComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.mode = "create";
   }
 
-  updateCurrentTeamMember(skill: skill) {
+  addTeamMember() {
+
+    if(!this.currentPlayer.name){
+      alert("Name cannot be null");
+      return;
+    }
+
+    this.team.players.push(this.currentPlayer);
+    this.reset();
+  }
+
+  updateSkills(skill: skill) {
 
     let index: number = this.currentPlayer.talents.indexOf(skill);
 
     if (index !== -1) {
-      this.currentPlayer.talents.splice(index, 1)
+      this.currentPlayer.talents.splice(index, 1);
     } else {
       this.currentPlayer.talents.push(skill);
     }
   }
 
-  addTeamMember() {
+  updateMember(player: player){
 
-    this.team.players.push(this.currentPlayer);
+    let index: number = this.team.players.indexOf(player);
+
+    this.team.players[index] = player;
     this.reset();
+    
+  }
+
+  removeMember(player:player){
+
+    let index: number = this.team.players.indexOf(player);
+    
+    if(index !== -1){
+      this.team.players.splice(index, 1);
+    };
+
+    this.reset();
+
+  }
+
+  editPlayer(player: player) {
+    this.currentPlayer = player;
+    this.mode = "edit";
+    let i;
+
+    if (player.talents.length > 0) {
+      player.talents.forEach((x) => {
+        i = this.skillsList.indexOf(x);
+        this.skillsList[i].checked = true;
+      });
+    }
 
   }
 
@@ -52,6 +93,8 @@ export class TeamMapComponent implements OnInit {
     })
 
     this.currentPlayer = {};
+
+    this.mode = "create";
   }
 
 }
