642eac54c41c2a27575a02438f2ae11378e38e14
Devin <devin.biggers@gmail.com>
2017-09-05 10:18:37 -0500

    Commenting out proxy for html2canvas

--- thinkingTalents/src/app/team-map/team-map.component.ts
+++ thinkingTalents/src/app/team-map/team-map.component.ts
@@ -238,7 +238,7 @@ export class TeamMapComponent implements OnInit {
 
     html2canvas(jquery("#teamMap")[0], {
       dpi: 192,
-      proxy: "http://localhost:4200",
+      //proxy: "http://localhost:4200",
       logging: true,
     }).then((canvas) => {
 
